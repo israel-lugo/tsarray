7ea3c83c6ad91fc714a540094e823907890e26de
Israel G. Lugo <israel.lugo@lugosys.com>
2017-08-31 03:19:30 +0100

    Create test suite using libcheck, check-tsarray. Still WIP.

---
+++ tests/check-tsarray.c
@@ -0,0 +1,86 @@
+/*
+ * tsarray - type-safe dynamic array library
+ * Copyright 2012, 2015, 2016, 2017 Israel G. Lugo
+ *
+ * This file is part of tsarray.
+ *
+ * tsarray is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tsarray is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tsarray.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For suggestions, feedback or bug reports: israel.lugo@lugosys.com
+ */
+
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <check.h>
+
+#include <tsarray.h>
+
+
+TSARRAY_TYPEDEF(intarray, int);
+
+
+
+START_TEST(test_create_and_free)
+{
+    intarray a1 = TSARRAY_INITIALIZER;
+
+    ck_assert(a1.len == 0);
+    ck_assert(a1._priv.capacity >= 0);
+    ck_assert((a1.items == NULL) == (a1._priv.capacity == 0));
+
+    intarray_free(&a1);
+}
+END_TEST
+
+
+
+Suite *foo_suite(void)
+{
+    Suite *s;
+    TCase *tc;
+
+    s = suite_create("tsarray");
+
+    tc = tcase_create("core");
+
+    tcase_add_test(tc, test_create_and_free);
+    suite_add_tcase(s, tc);
+
+    return s;
+}
+
+
+int main(void)
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = foo_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+
+    srunner_free(sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+
+/* vim: set expandtab smarttab shiftwidth=4 softtabstop=4 tw=75 : */
